598e8eba7511bddda84c3e8c373ceeaf5f01162a
Tuan <tv@vergelink.io>
2023-12-18 10:49:03 +0100

    Add Skeletons

---
+++ app/(auth)/forgotpassword.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const ForgotPassword = (props: Props) => {
+  return (
+    <div>ForgotPassword
+    
+    </div>
+  )
+}
+
+export default ForgotPassword
---
+++ app/(auth)/onboarding.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Onboarding = (props: Props) => {
+  return (
+    <div>Onboarding
+    
+    </div>
+  )
+}
+
+export default Onboarding
